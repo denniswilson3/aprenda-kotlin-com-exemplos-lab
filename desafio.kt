3786d9529af32ae784ad35d8e189d6a7bf441646
denniswilson3 <41599459+denniswilson3@users.noreply.github.com>
2023-01-11 22:51:15 -0300

    Resolução do Desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,83 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+data class Usuario(val nome: String, val email: String) {
+    override fun toString(): String {
+        return "${this.nome} - ${this.email}"
+    }
+}
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val nivel: Nivel, val duracao: Int = 60) {
+    override fun toString(): String {
+        return "${this.nome} - ${this.nivel} - ${this.duracao}"
+    }
+}
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val nivel: Nivel, val conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+    }
+    
+    fun imprimirInscritos(){
+        for(usuario in inscritos){
+            println(usuario)
+        }
+    }
+    
+    fun imprimirConteudos(){
+        for(conteudo in conteudos){
+            println(conteudo)
+        }
+    }
+    
+    private fun formatarConteudosParaImpressao(): String {
+        var str = ""
+        for(conteudo in conteudos){
+            str += conteudo
+            str += "\n"
+        }
+        return str
+    }
+    
+    private fun formatarUsuariosParaImpressao(): String {
+    	var str = ""
+        for(usuario in inscritos){
+            str += usuario
+            str += "\n"
+        }
+        return str
+    }
+    
+    override fun toString(): String {
+        var str = "############## FORMAÇÃO ##############\n"
+        str += "Nome: ${this.nome}\n"
+        str += "Nível: ${this.nivel}\n"
+        str += "############## CONTEÚDOS ##############\n"
+      	str += formatarConteudosParaImpressao()
+        str += "############## USUÁRIOS INSCRITOS ##############\n"
+        str += formatarUsuariosParaImpressao()
+        return str
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val conteudosEducacionaisJava: List<ConteudoEducacional> = 
+    	listOf(ConteudoEducacional("Pricipais Protocolos de Comunicação da Internet", Nivel.BASICO, 180),
+               ConteudoEducacional("Imersão no Spring Framework com Spring Boot", Nivel.INTERMEDIARIO, 180),
+               ConteudoEducacional("Explorando Padrões de Projeto na Prática com Java", Nivel.AVANCADO, 120))
+        
+    val formacaoJavaDeveloper: Formacao = Formacao("Formação Java Developer", Nivel.INTERMEDIARIO, conteudosEducacionaisJava)
+        
+    val usuarios: List<Usuario> = listOf(Usuario("Denilson", "denilson@gmail.com"),
+                                         Usuario("Julia", "julia@gmail.com"),
+                                         Usuario("Marisa", "marisa@gmail.com"))
+    for(usuario in usuarios){
+        formacaoJavaDeveloper.matricular(usuario)
+    }
+    
+    print(formacaoJavaDeveloper)
 }
